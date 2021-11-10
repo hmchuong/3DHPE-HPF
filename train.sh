c0f92a76806030e92779004d6d22ebec17203415
chuonghm <chuonghm@umd.edu>
2021-11-10 21:56:24 +0000

    training script

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python run_poseformer_gen_data.py -k combine -f 81 -lr 0.00004 -lrd 0.99 -c checkpoint --resume detected81f.bin
+python run_poseformer_gen_data.py -k combine -f 81 -lr 0.001 --batch-size 4096 -lrd 0.99 -c checkpoint --resume detected81f.bin
